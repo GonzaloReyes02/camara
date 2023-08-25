54858a4dc153b65f550a9b28101b3eaa8d9dda2f
GonzaloReyes02 <138870732+GonzaloReyes02@users.noreply.github.com>
2023-08-25 12:11:08 +0200

    Add files via upload

---
+++ capture_images/README.md
@@ -0,0 +1,12 @@
+# Capturador de Imagenes
+La forma en que Android delega acciones a otras aplicaciones es mediante la invocación de un Intent que describe lo que deseas que se haga. Este proceso tiene tres partes: el Intent en sí, una llamada para iniciar la Activity externa y un poco de código para manejar los datos de la imagen cuando el foco vuelve a tu actividad
+
+
+
+Este ejemplo ha sido creado para los vídeos de YouTube:
+- [Deprecated startActivityForResult](https://www.youtube.com/watch?v=DSn8IVMvRJ0 "Deprecated startActivityForResult")
+- [Captura de imagenes Android Kotlin](https://www.youtube.com/watch?v=Srp6e5LK8WI "Captura de imagenes Android Kotlin")
+- [Guardar imágenes en la gelería Android Kotlin](https://www.youtube.com/watch?v=pSKFbToSHGM "Guardar imágenes en la gelería Android Kotlin")
+
+
+
---
+++ capture_images/app/src/main/java/com/codigonline/android/previa/result/MainActivity.kt
@@ -0,0 +1,109 @@
+package com.codigonline.android.previa.result
+
+import android.content.ContentValues
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import com.codigonline.android.previa.result.databinding.ActivityMainBinding
+import java.io.File
+import java.io.OutputStream
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.openCamera.setOnClickListener {
+
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {
+                it.resolveActivity(packageManager).also { component ->
+                    createPhotoFile()
+                    val photoUri: Uri =
+                        FileProvider.getUriForFile(
+                            this,
+                            BuildConfig.APPLICATION_ID + ".fileprovider", file
+                        )
+                    it.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+                }
+            }
+            openCamera.launch(intent)
+            //openCamera.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+        }
+
+        binding.saveToGallery.setOnClickListener {
+            saveToGallery()
+        }
+
+    }
+
+
+    private val openCamera =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                // val data = result.data!!
+                // val bitmap = data.extras!!.get("data") as Bitmap
+                val bitmap = getBitmap()
+                binding.img.setImageBitmap(bitmap)
+
+            }
+        }
+    private lateinit var file: File
+    private fun createPhotoFile() {
+        val dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+        file = File.createTempFile("IMG_${System.currentTimeMillis()}_", ".jpg", dir)
+    }
+
+    private fun saveToGallery() {
+        val content = createContent()
+        val uri = save(content)
+        clearContents(content, uri)
+        Toast.makeText(this,getString(R.string.image_saved),Toast.LENGTH_LONG).show()
+    }
+
+    private fun createContent(): ContentValues {
+        val fileName = file.name
+        val fileType = "image/jpg"
+        return ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+            put(MediaStore.Files.FileColumns.MIME_TYPE, fileType)
+            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+            put(MediaStore.MediaColumns.IS_PENDING, 1)
+        }
+    }
+
+    private fun save(content: ContentValues): Uri {
+        var outputStream: OutputStream?
+        var uri: Uri?
+        application.contentResolver.also { resolver ->
+            uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, content)
+            outputStream = resolver.openOutputStream(uri!!)
+        }
+        outputStream.use { output ->
+            getBitmap().compress(Bitmap.CompressFormat.JPEG, 100, output)
+        }
+        return uri!!
+    }
+
+    private fun clearContents(content: ContentValues, uri: Uri) {
+        content.clear()
+        content.put(MediaStore.MediaColumns.IS_PENDING,0)
+        contentResolver.update(uri,content,null,null)
+    }
+
+    private fun getBitmap(): Bitmap {
+        return BitmapFactory.decodeFile(file.toString())
+    }
+
+
+}
